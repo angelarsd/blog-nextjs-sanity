703cd28bb0f8f8ecad6017c7f8a693c75fe831a0
Ángel Silva <angel.silva@tunubi.com>
2024-12-27 17:47:46 -0300

    Se agregó las constantes

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const EXAMPLE_PATH = 'cms-sanity'
+export const CMS_NAME = 'Blog - Susana Dallal'
+export const CMS_URL = 'blog.susanadallal.com'
+export const PAGE_URL = 'www.susanadallal.com'
